fcabd427aecc7ab7a6e69d5499bea0cdbba43387
Krishna Wishvajith <krishnawishvajith@gmail.com>
2025-06-12 15:54:12 +0530

    Update Core.php

--- app/libraries/Core.php
+++ app/libraries/Core.php
@@ -6,7 +6,22 @@
          protected $param = [];
 
          public function __construct(){
-            print_r($this->getURL());
+            //print_r($this->getURL());
+            $url = $this->getURL();
+
+            if(file_exists('../app/controllers/'.ucwords($url[0]).'.php')){
+            //if the contoroller exists, then load it
+            $this->currentController = ucwords($url[0]);
+
+            //unset the controller in the URL
+            unset($url[0]);
+            }
+
+            //call the controller
+            require_once '../app/controllers/'.$this->currentController.'.php';
+
+            // Instantiate the controller 
+            $this->currentController = new $this->currentController;
          }
 
          public function getURL(){
